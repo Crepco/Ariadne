0b7ff89bce413368a05e036fad500b0f7f217738
Humera-tech <hmahreen3105@gmail.com>
2026-08-10 22:36:14 +0530

    Pages connections Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import LandingPage from "./Pages/LandingPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./Pages/LandingPage";
+import Demo from "./Pages/Demo";
+import BenchmarkPage from "./Pages/BenchmarkPage";
+import Navbar from "./components/LandingPage/Navbar";
 
 const App = () => {
   return (
-    <div >
-      <LandingPage />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="min-h-screen bg-[#020817] text-white">
+        <Navbar />
 
-export default App
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/benchmark" element={<BenchmarkPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
