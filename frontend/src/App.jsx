b179d4fbbdae9bbdf43a1802a6351f1b8f154f36
Humera-tech <hmahreen3105@gmail.com>
2026-08-11 23:46:35 +0530

    Connections done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import Demo from "./Pages/Demo";
 import BenchmarkPage from "./Pages/BenchmarkPage";
+
 import Navbar from "./components/LandingPage/Navbar";
+import WebAppLayout from "./components/WebApp/WebAppLayout";
+
+import Dashboard from "./Pages/WebApp/Dashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[#020817] text-white">
-        <Navbar />
 
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/demo" element={<Demo />} />
-          <Route path="/benchmark" element={<BenchmarkPage />} />
+
+          <Route path="/" element={<><Navbar /><LandingPage /></>} />
+
+          <Route path="/demo" element={<><Navbar /><Demo /></>} />
+
+          <Route path="/benchmark" element={<><Navbar /><BenchmarkPage /></>} />
+
+          <Route path="/app" element={<WebAppLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+
         </Routes>
+
       </div>
     </BrowserRouter>
   );
